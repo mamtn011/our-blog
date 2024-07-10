1467780bbb8d551f9628d0d78fcd5486acae4cc2
Mobin <mamtn011@gmail.com>
2024-07-11 02:02:21 +0600

    completed header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./app.css";
 import Header from "@/components/common/Header";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Header /> */}
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/common/Header.js
@@ -0,0 +1,97 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+// react icon
+import {
+  FaBars,
+  FaXmark,
+  FaSquareFacebook,
+  FaSquareDribbble,
+  FaSquareXTwitter,
+  FaSquareGithub,
+} from "react-icons/fa6";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const pathname = usePathname();
+  const navItems = [
+    { path: "/", label: "Home" },
+    { path: "/about", label: "About" },
+    { path: "/blogs", label: "Blog" },
+    { path: "/contact", label: "Contact" },
+  ];
+  return (
+    <header className="bg-black text-white fixed top-0 left-0 right-0">
+      <nav className="p-4 max-w-7xl mx-auto flex justify-between items-center">
+        {/* logo  */}
+        <Link href="/" className="text-xl font-bold text-white">
+          My<span className="text-orange-500">Blog</span>
+        </Link>
+        {/* nav items for lg device */}
+        <ul className="md:flex gap-12 text-lg hidden">
+          {navItems.map(({ path, label }) => (
+            <li
+              key={path}
+              className="text-white hover:underline underline-offset-4"
+            >
+              <Link href={path} className={pathname === path ? "active" : ""}>
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        {/* menu icons */}
+        <div className="text-white lg:flex gap-4 items-center hidden">
+          <Link href="/" className="hover:text-orange-500">
+            <FaSquareFacebook />
+          </Link>
+          <Link href="/" className="hover:text-orange-500">
+            <FaSquareDribbble />
+          </Link>
+          <Link href="/" className="hover:text-orange-500">
+            <FaSquareXTwitter />
+          </Link>
+          <Link href="/" className="hover:text-orange-500">
+            <FaSquareGithub />
+          </Link>
+        </div>
+
+        {/* mobile menu btn - display on mobile screen */}
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="cursor-pointer">
+            {isMenuOpen ? (
+              <FaXmark className="w-5 h-5" />
+            ) : (
+              <FaBars className="w-5 h-5" />
+            )}
+          </button>
+        </div>
+      </nav>
+      {/* nav items for mobile device */}
+      <ul
+        className={`md:hidden gap-12 text-lg block space-y-2 p-4 mt-14 bg-white ${
+          isMenuOpen
+            ? "fixed top-0 left-0 w-full transition-all ease-out duration-150"
+            : "hidden"
+        }`}
+      >
+        {navItems.map(({ path, label }) => (
+          <li key={path} className="text-black">
+            <Link
+              href={path}
+              className="hover:text-orange-500 hover:underline underline-offset-4"
+              onClick={toggleMenu}
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
